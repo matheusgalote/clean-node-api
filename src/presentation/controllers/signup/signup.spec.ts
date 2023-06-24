e533bf24f6d5f5240dd33a56c9a2648466bc4fe2
matheusgalote <mateugalote@gmail.com>
2023-06-24 10:36:02 -0300

    test: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -208,4 +208,25 @@ describe('SignUp Controller', () => {
       password: 'joedoe'
     })
   })
+
+  test('Should 500 if AddAccount throws', () => {
+    const { sut, addAccountStub } = makeSut()
+
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpRequest = {
+      body: {
+        name: 'Joe Doe',
+        email: 'joedoe@email.com',
+        password: 'joedoe',
+        passwordConfirmation: 'joedoe'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
