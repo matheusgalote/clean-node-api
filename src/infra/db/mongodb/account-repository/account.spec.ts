d6f8f701cebec07688f7c9306d5efc7504c15f22
matheusgalote <mateugalote@gmail.com>
2023-07-03 21:15:18 -0300

    refactor: add sut helper factory method

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,8 +10,15 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return an account on success', async () => {
+  const makeSut = (): AccountMongoRepository => {
     const sut = new AccountMongoRepository()
+
+    return sut
+  }
+
+  test('Should return an account on success', async () => {
+    const sut = makeSut()
+
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@email.com',
