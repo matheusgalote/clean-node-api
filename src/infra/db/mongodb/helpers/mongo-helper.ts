75453578132eca73b7ce445289fe50ea9bfdcef9
matheusgalote <mateugalote@gmail.com>
2023-07-03 21:12:39 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,11 +1,17 @@
-import { MongoClient } from 'mongodb'
+import { type Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect (url: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__)
+    this.client = await MongoClient.connect(process.env.MONGO_URL)
   },
+
   async disconnect () {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
