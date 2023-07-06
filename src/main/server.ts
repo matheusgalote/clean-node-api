4c12ed724139afb840aaa2e9b7ebbf58a5085936
matheusgalote <mateugalote@gmail.com>
2023-07-05 21:51:40 -0300

    refactor: set 3333 as defult port for server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,3 @@
 import app from './config/app'
 
-app.listen(3000, () => { console.log('Server running at http://localhost:3000') })
+app.listen(3333, () => console.log('Server running at http://localhost:3333'))
