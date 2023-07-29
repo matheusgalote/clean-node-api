76243238df132959b90aa607d4640c26dd3ad868
matheusgalote <mateugalote@gmail.com>
2023-07-28 22:03:46 -0300

    feat: ensure return error when email is not valid

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import type { HttpRequest, HttpResponse, Controller } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { type EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
@@ -19,6 +19,10 @@ export class LoginController implements Controller {
       return badRequest(new MissingParamError('password'))
     }
 
-    this.emailValidator.isValid(httpRequest.body.email)
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
+    }
   }
 }
