4cd82d5f10ead455480e8ffd9c51c506eff88bf3
matheusgalote <mateusgalote@gmail.com>
2023-08-28 20:25:53 -0300

    test: ensure AccountMongoRepository retuns null if loadAccountByEmail returns null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -54,4 +54,10 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@email.com')
     expect(account.password).toBe('any_password')
   })
+
+  test('Should return null if loadEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@email.com')
+    expect(account).toBeFalsy()
+  })
 })
