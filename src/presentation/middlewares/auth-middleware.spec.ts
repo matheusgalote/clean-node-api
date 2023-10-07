68bf81045ea5c2f262b265ce3b0d4e2e9bc495f5
Matheus <mateusgalote@gmail.com>
2023-10-07 16:31:31 -0300

    refactor: move sut creation to a factory method

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -20,17 +20,31 @@ const makeLoadAccount = (): LoadAccountByToken => {
   return new LoadAccountByTokenStub()
 }
 
+interface ISut {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeSut = (): ISut => {
+  const loadAccountByTokenStub = makeLoadAccount()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+
+  return {
+    sut,
+    loadAccountByTokenStub
+  }
+}
+
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware(makeLoadAccount())
+    const { sut } = makeSut()
     const error = await sut.handle({})
     expect(error).toEqual(forbidden(new AccessDeniedError()))
   })
 
   test('Should call LoadAccountByToken with correct accessToken', async () => {
-    const loadAccountByTokenStub = makeLoadAccount()
+    const { sut, loadAccountByTokenStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
-    const sut = new AuthMiddleware(loadAccountByTokenStub)
     await sut.handle({
       headers: {
         'x-access-token': 'any_token'
